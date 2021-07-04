4d9ff2255afa3e3b33e3a35c634f04f31077256f
matrix <64998320+shnider0@users.noreply.github.com>
2021-07-04 17:51:05 +0300

    Add files via upload

---
+++ VCsMusicBot/config.py
@@ -0,0 +1,28 @@
+from os import getenv
+import os
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "qad3im")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/f143fb3aa1115395315b6.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VCsMusicPlayer")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "qad3im")
+PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
+SOURCE_CODE = getenv("SOURCE_CODE", "t.me/qad3im")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
---
+++ VCsMusicBot/function/__init__.py
@@ -0,0 +1,5 @@
+from VCsMusicBot.function.admins import admins
+from VCsMusicBot.function.admins import get
+from VCsMusicBot.function.admins import set
+
+__all__ = ["set", "get", "admins"]
